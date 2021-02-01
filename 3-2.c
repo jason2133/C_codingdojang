b1af25727f96487331ce21dfca1f40f1a59b8762
Jason Lee <jason2133@likelion.org>
2021-02-01 20:52:54 +0900

    기본적인 C언어 코드, 자료형 형태, Input 입력받기

---
+++ 3-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("%s\n", "Hello World!");
+    // %s는 서식지정자
+    // Hello World가 %s 안으로 들어간다.
+
+    printf("%s %s\n", "Hello", "Jason");
+
+    return 0;
+}
+
---
+++ 7-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char num1 = -10;
+    short num2 = 30000;
+    int num3 = -123123;
+    long num4 = 121241;
+    long long num5 = 345325;
+
+    printf("%d %d %d %ld %lld\n", num1, num2, num3, num4, num5);
+
+    return 0;
+}
+
